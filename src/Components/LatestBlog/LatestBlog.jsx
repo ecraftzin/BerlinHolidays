d8ac00da269701fb7f8a86ae61661d30df4954f7
MeenakshyVAyyappan <meenakshyyvp310@gmail.com>
2025-11-10 18:25:20 +0530

    home page blog update

--- src/Components/LatestBlog/LatestBlog.jsx
+++ src/Components/LatestBlog/LatestBlog.jsx
@@ -1,9 +1,15 @@
+import { useState, useEffect } from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { useKeenSlider } from "keen-slider/react";
+import { getPublishedBlogPosts } from "../../services";
 import "../../Components4/Testimonial/testimonials.css";
 import "keen-slider/keen-slider.min.css";
+
 const LatestBlog = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const [sliderRef] = useKeenSlider({
     breakpoints: {
       "(min-width: 320px)": {
@@ -19,17 +25,35 @@ const LatestBlog = () => {
     loop: true,
     initial: 0,
   });
+
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, []);
+
+  const fetchLatestBlogs = async () => {
+    setLoading(true);
+    const { data, error } = await getPublishedBlogPosts();
+    
+    if (!error && data) {
+      setBlogs(data.slice(0, 6));
+    }
+    
+    setLoading(false);
+  };
+
+  if (!loading && blogs.length === 0) {
+    return null;
+  }
+
   return (
     <div className="dark:bg-normalBlack">
       <div className="bg-[url('/images/home-1/section-shape2.png')] bg-no-repeat bg-top bg-opacity-[0.07]">
         <section className="Container py-20 lg:py-[120px]">
-          {/* Section Header */}
           <div
             data-aos="fade-up"
             data-aos-duration="1000"
             className=" text-center mx-auto  px-5 sm:px-8 md:px-[80px] lg:px-[120px] xl:px-[200px] 2xl:px-[335px]"
           >
-            {/* Section logo */}
             <div className="flex items-center justify-center space-x-2 mb-4  ">
               <hr className="w-[100px] h-[1px] text-[#dedbd4] dark:text-[#3b3b3b] " />
               <img
@@ -48,164 +72,73 @@ const LatestBlog = () => {
                and insider tips that make every journey unforgettable.
             </p>
           </div>
-          {/* all blogs are here */}
           <div className="relative">
-            <div className="mt-14 2xl:mt-[60px] keen-slider" ref={sliderRef}>
-              {/* slide - 1 */}
-              <div className="keen-slider__slide number-slide1 ">
-                {/* card one */}
-                <div
-                  className="overflow-hidden 3xl:w-[410px] group"
-                  data-aos="fade-up-left"
-                  data-aos-duration="1000"
-                >
-                  <div className="relative">
-                    <img
-                      src="/images/home-1/blog1img.png"
-                      className="w-full h-full object-cover"
-                      alt=""
-                    />
-                  </div>
-                  <div className="font-Garamond border border-[#e8e8e8] dark:border-[#424242] border-t-0">
-                    <div className="py-6 px-[30px] lg:px-5 xl:px-[25px] ">
-                      <div className="flex items-center space-x-6">
-                        {/* <p className="text-sm lg:text-base leading-[26px] text-gray dark:text-lightGray font-normal uppercase mr-7 ml-3 relative before:absolute before:w-[7px] before:h-[7px] before:left-[-13px] before:bg-[#d1d1d1] dark:before:bg-khaki before:top-[9px]">
-                          August 10, 2023
-                        </p> */}
-                        {/* <p className="text-sm lg:text-base leading-[26px] text-gray dark:text-lightGray font-normal uppercase mr-7 ml-3 relative before:absolute before:w-[7px] before:h-[7px] before:left-[-13px] before:bg-[#d1d1d1] dark:before:bg-khaki before:top-[9px]">
-                          Interior
-                        </p> */}
-                      </div>
-                      <Link to="/blog">
-                        <button className="text-xl sm:text-[22px] xl:text-2xl text-left 2xl:text-[26px] leading-[34px] font-semibold text-lightBlack dark:text-white py-2 sm:py-3 md:py-4 hover:underline underline-offset-2">
-                         Top 5 Luxury Resorts in Wayanad for Your Next Getaway
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="  border-t-[1px] border-[#e8e8e8] dark:border-[#424242]  py-2 lg:py-3">
-                      <Link
-                        to={"blog"}
-                        className="px-[30px] flex items-center justify-between "
-                      >
-                        <button className=" text-sm sm:text-base flex items-center ">
-                          <span className="ml-[10px] leading-[38px] uppercase text-lightBlack dark:text-white font-medium group-hover:text-khaki hover:underline  underline-offset-1">
-                            Read More
-                          </span>
-                        </button>
-                        <div>
-                          <BsArrowRight
-                            className="text-gray dark:text-lightGray group-hover:text-khaki"
-                            size={"24px"}
-                          />
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
+            {loading ? (
+              <div className="text-center py-20">
+                <div className="inline-block animate-spin rounded-full h-12 w-12 border-b-2 border-khaki"></div>
+                <p className="mt-4 text-gray dark:text-lightGray font-Lora">Loading latest posts...</p>
               </div>
-              {/* slide - 2 */}
-              <div className="keen-slider__slide number-slide1 ">
-                <div
-                  className="overflow-hidden 3xl:w-[410px] group"
-                  data-aos="fade-up"
-                  data-aos-duration="1000"
-                >
-                  <div className="relative">
-                    <img
-                      src="/images/home-1/blog2img.png "
-                      className="w-full h-full object-cover"
-                      alt=""
-                    />
-                  </div>
-                  <div className="font-Garamond border border-[#e8e8e8] dark:border-[#424242] border-t-0">
-                    <div className="py-6 px-[30px] lg:px-5 xl:px-[25px] ">
-                      <div className="flex items-center space-x-6">
-                        {/* <p className="text-sm lg:text-base leading-[26px] text-gray dark:text-lightGray font-normal uppercase mr-7 ml-3 relative before:absolute before:w-[7px] before:h-[7px] before:left-[-13px] before:bg-[#d1d1d1] dark:before:bg-khaki before:top-[9px]">
-                          August 10, 2023
-                        </p> */}
-                        {/* <p className="text-sm lg:text-base leading-[26px] text-gray dark:text-lightGray font-normal uppercase mr-7 ml-3 relative before:absolute before:w-[7px] before:h-[7px] before:left-[-13px] before:bg-[#d1d1d1] dark:before:bg-khaki before:top-[9px]">
-                          Interior
-                        </p> */}
+            ) : (
+              <div className="mt-14 2xl:mt-[60px] keen-slider" ref={sliderRef}>
+                {blogs.map((blog, index) => (
+                  <div key={blog.id} className="keen-slider__slide number-slide1">
+                    <div
+                      className="overflow-hidden 3xl:w-[410px] group"
+                      data-aos={index === 0 ? "fade-up-left" : index === 1 ? "fade-up" : "fade-up-right"}
+                      data-aos-duration="1000"
+                    >
+                      <div className="relative">
+                        <img
+                          src={blog.featured_image || "/images/home-1/blog1img.png"}
+                          className="w-full h-full object-cover"
+                          alt={blog.title}
+                          onError={(e) => {
+                            e.target.src = "/images/home-1/blog1img.png";
+                          }}
+                        />
+                        {blog.category_name && (
+                          <div className="absolute top-4 left-4 bg-khaki text-white px-3 py-1 text-xs font-Lora font-semibold uppercase">
+                            {blog.category_name}
+                          </div>
+                        )}
                       </div>
-                      <Link to="/blog">
-                        <button className="text-xl sm:text-[22px] xl:text-2xl text-left 2xl:text-[26px] leading-[34px] font-semibold text-lightBlack dark:text-white py-2 sm:py-3 md:py-4 hover:underline underline-offset-2">
-                          A Taste of Luxury: Fine Dining Experiences You Can’t Miss
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="  border-t-[1px] border-[#e8e8e8] dark:border-[#424242]  py-2 lg:py-3">
-                      <Link
-                        to={"blog"}
-                        className="px-[30px] flex items-center justify-between "
-                      >
-                        <button className=" text-sm sm:text-base flex items-center ">
-                          <span className="ml-[10px] leading-[38px] uppercase text-lightBlack dark:text-white font-medium group-hover:text-khaki hover:underline  underline-offset-1">
-                            Read More
-                          </span>
-                        </button>
-                        <div>
-                          <BsArrowRight
-                            className="text-gray dark:text-lightGray group-hover:text-khaki"
-                            size={"24px"}
-                          />
+                      <div className="font-Garamond border border-[#e8e8e8] dark:border-[#424242] border-t-0">
+                        <div className="py-6 px-[30px] lg:px-5 xl:px-[25px]">
+                          <Link to={`/blog/${blog.id}`}>
+                            <button className="text-xl sm:text-[22px] xl:text-2xl text-left 2xl:text-[26px] leading-[34px] font-semibold text-lightBlack dark:text-white py-2 sm:py-3 md:py-4 hover:underline underline-offset-2">
+                              {blog.title}
+                            </button>
+                          </Link>
+                          {blog.excerpt && (
+                            <p className="text-sm text-gray dark:text-lightGray font-Lora mt-2 line-clamp-2">
+                              {blog.excerpt}
+                            </p>
+                          )}
                         </div>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* slide - 3 */}
-              <div className="keen-slider__slide number-slide1 ">
-                <div
-                  className="overflow-hidden 3xl:w-[410px] group"
-                  data-aos="fade-up-right"
-                  data-aos-duration="1000"
-                >
-                  <div className="relative">
-                    <img
-                      src="/images/home-1/blog3img.png"
-                      className="w-full h-full object-cover"
-                      alt=""
-                    />
-                  </div>
-                  <div className="font-Garamond border border-[#e8e8e8] dark:border-[#424242] border-t-0">
-                    <div className="py-6 px-[30px] lg:px-5 xl:px-[25px] ">
-                      <div className="flex items-center space-x-6">
-                        {/* <p className="text-sm lg:text-base leading-[26px] text-gray dark:text-lightGray font-normal uppercase mr-7 ml-3 relative before:absolute before:w-[7px] before:h-[7px] before:left-[-13px] before:bg-[#d1d1d1] dark:before:bg-khaki before:top-[9px]">
-                          August 10, 2023
-                        </p> */}
-                        {/* <p className="text-sm lg:text-base leading-[26px] text-gray dark:text-lightGray font-normal uppercase mr-7 ml-3 relative before:absolute before:w-[7px] before:h-[7px] before:left-[-13px] before:bg-[#d1d1d1] dark:before:bg-khaki before:top-[9px]">
-                          Interior
-                        </p> */}
+                        <div className="border-t-[1px] border-[#e8e8e8] dark:border-[#424242] py-2 lg:py-3">
+                          <Link
+                            to={`/blog/${blog.id}`}
+                            className="px-[30px] flex items-center justify-between"
+                          >
+                            <button className="text-sm sm:text-base flex items-center">
+                              <span className="ml-[10px] leading-[38px] uppercase text-lightBlack dark:text-white font-medium group-hover:text-khaki hover:underline underline-offset-1">
+                                Read More
+                              </span>
+                            </button>
+                            <div>
+                              <BsArrowRight
+                                className="text-gray dark:text-lightGray group-hover:text-khaki"
+                                size={"24px"}
+                              />
+                            </div>
+                          </Link>
+                        </div>
                       </div>
-                      <Link to="/blog">
-                        <button className="text-xl sm:text-[22px] xl:text-2xl text-left 2xl:text-[26px] leading-[34px] font-semibold text-lightBlack dark:text-white py-2 sm:py-3 md:py-4 hover:underline underline-offset-2">
-                          Wellness Retreats: Reconnect, Relax, and Recharge
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="  border-t-[1px] border-[#e8e8e8] dark:border-[#424242]  py-2 lg:py-3">
-                      <Link
-                        to={"blog"}
-                        className="px-[30px] flex items-center justify-between "
-                      >
-                        <button className=" text-sm sm:text-base flex items-center ">
-                          <span className="ml-[10px] leading-[38px] uppercase text-lightBlack dark:text-white font-medium group-hover:text-khaki hover:underline  underline-offset-1">
-                            Read More
-                          </span>
-                        </button>
-                        <div>
-                          <BsArrowRight
-                            className="text-gray dark:text-lightGray group-hover:text-khaki"
-                            size={"24px"}
-                          />
-                        </div>
-                      </Link>
                     </div>
                   </div>
-                </div>
+                ))}
               </div>
-            </div>
+            )}
           </div>
         </section>
       </div>
